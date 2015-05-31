5cd2ffa9caa1bfe5f7af2d1c16a58b2bafe7aced
Kosei-A <kouchei@gmail.com>
2015-05-31 13:03:42 +0900

    Mod README.md & helloworld.py

--- README.md
+++ README.md
@@ -4,8 +4,20 @@
 just for practice
 
 ### Language
-* python
+* python3
 * perl
 * c
 * Java
 * HTML+javascript
+
+### Contents
+#### Hello World
+ 1.1 print only 1 time
+ 1.2 loop 5 times
+ 1.3 print each characters
+ 1.4 print [l] character only
+ 1.5 print first 4 characters
+ 1.6 print last 4 characters
+ 1.7 print 4 characters from [W]
+
+ 
--- python3.4/helloworld.py
+++ python3.4/helloworld.py
@@ -1,7 +1,7 @@
 #---------------------------------------------------
 # Name:        helloworld
 # Purpose:     Practice
-# Python version : 3.4
+# version :    1.0
 # Author:      kosei.a
 #
 # Created:     26/5/2015
@@ -10,37 +10,41 @@
 #---------------------------------------------------
 
 def main():
+    print("----- 1.1 only 1 time -----")
     print("Hello World")
-    print("End\n")
+    print("----- End -----\n")
 
-    print("- loop 5 times -")
+    print("----- 1.2 loop 5 times -----")
     n = 5
     for i in range(n):
         print("Hello World")
-    print("End\n")
+    print("----- End -----\n")
 
-    print("- each word -")
+    print("----- 1.3 print each characters -----")
     for str in "Hello World":
         print(str)
-    print("End\n")
+    print("----- End -----\n")
 
-    print("- l only -")
+    print("----- 1.4 print [l] character only -----")
     for str in "Hello World":
         if str == "l":
             print(str)
-    print("End\n")
+    print("----- End -----\n")
 
-    print("- RIGHT -")
+    print("----- 1.5 print first 4 characters -----")
     str = "Hello World"
     print("{0:s}\n".format(str[:4]))
+    print("----- End -----\n")
 
-    print("- LEFT -")
+    print("----- 1.6 print last 4 characters -----")
     str = "Hello World"
     print("{0:s}\n".format(str[-4:]))
+    print("----- End -----\n")
 
-    print("- 4word from W -")
+    print("----- 1.7 print 4 characters from [W] -----")
     beg = (str.find("W"))
     print("{0:s}\n".format(str[beg:beg+4]))
+    print("----- End -----\n")
 
 if __name__ == '__main__':
     main()
