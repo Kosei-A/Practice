eb75ebd48cd531dd3081471a313c5fa7aca679ca
Kosei-A <kouchei@gmail.com>
2016-02-14 05:18:13 +0900

    modify csvopen.py

---
+++ python3.4/csvopen.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+#---------------------------------------------------
+# Name:        csv open
+# Purpose:     lesson
+# Python version : 3.4
+# Author:      kosei.a
+#
+# Created:     13/2/2016
+# Copyright:   kosei.a
+# Licence:     your licence
+# Command:     python3 csvopen.py -f [filename]
+#---------------------------------------------------
+
+import csv            #csv module import
+import argparse       #argparse module import
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', action='store', dest='file_name',
+                    help='Store a file name',required = True)
+
+results = parser.parse_args()
+print ('')
+print ('file name     =', results.file_name)
+print ('')
+
+def main():
+    dest = results.file_name
+
+    delimiter=","     # <-好きな区切り文字を指定, csv fileはカンマ区切り
+    with open(dest, "rU") as f:
+        data = map(lambda x:x.split(delimiter), f.read().strip().split("\n"))
+
+    for row in data:
+       print (row[0], row[1], row[2], row[3])
+
+if __name__ == '__main__':
+    main()
+
+
+
--- python3.4/helloworld.py
+++ python3.4/helloworld.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 #---------------------------------------------------
 # Name:        helloworld
 # Purpose:     Practice
